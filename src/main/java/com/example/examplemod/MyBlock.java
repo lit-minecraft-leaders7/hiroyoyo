05b0afc06ad4b73cab11e0e845055f34530548c5
k014c1139 <a.a>
2016-02-07 16:43:28 +0900

    Init

---
+++ src/main/java/com/example/examplemod/MyBlock.java
@@ -0,0 +1,10 @@
+package com.example.examplemod;
+
+/**
+ * Created by k014c1139 on 2016/02/07.
+ */
+import net.minecraft.block.Block;
+import net.minecraft.block.meterial.Meterial;
+
+public class MyBlock {
+}
