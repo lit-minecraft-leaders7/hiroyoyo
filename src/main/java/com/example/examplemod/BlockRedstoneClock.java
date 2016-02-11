f1441beec3382153f0e09644f8b9b922bfd7d397
k014c1139 <a.a>
2016-02-12 04:58:27 +0900

    Init

---
+++ src/main/java/com/example/examplemod/BlockRedstoneClock.java
@@ -0,0 +1,57 @@
+package com.example.examplemod;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
+
+import java.util.Random;
+
+/**
+ * Created by k014c1139 on 2016/02/12.
+ */
+public class BlockRedstoneClock extends Block {
+    public BlockRedstoneClock(){
+        super(Material.rock);
+        setCreativeTab(CreativeTabs.tabRedstone);
+
+        setBlockName(ExampleMod.MODID + "BlockRedstoneClock");
+        setBlockTextureName(ExampleMod.MODID + ":" + "redstone_clock");
+
+        setHardness(100F);
+    }
+
+    @Override
+    public boolean canConnectRedstone(IBlockAccess world, int x, int y, int z, int side){
+        return true;
+    }
+
+    @Override
+    public boolean canProvidePower(){
+        return true;
+    }
+
+    @Override
+    public int isProvidingWeakPower(IBlockAccess world, int x, int y,int z, int size){
+        return world.getBlockMetadata(x,y,z);
+    }
+
+    @Override
+    public void updateTick(World world, int x, int y, int z, Random random){
+        int meta = world.getBlockMetadata(x,y,z);
+
+        if(meta == 0){
+            world.setBlockMetadataWithNotify(x,y,z,15,3);
+        }else{
+            world.setBlockMetadataWithNotify(x,y,z,0,3);
+        }
+        
+        world.scheduleBlockUpdateWithPriority(x,y,z,this,5,100);
+    }
+
+    @Override
+    public void onBlockAdded(World world, int x, int y, int z){
+        world.scheduleBlockUpdateWithPriority(x,y,z,this,5,100);
+    }
+}
--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -1,6 +1,7 @@
 package com.example.examplemod;
 
 import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraft.block.Block;
 import net.minecraft.init.Blocks;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
@@ -8,18 +9,33 @@ import cpw.mods.fml.common.event.FMLInitializationEvent;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.minecart.MinecartCollisionEvent;
 
 @Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
 public class ExampleMod
 {
     public static final String MODID = "examplemod";
     public static final String VERSION = "1.0";
+    public static final Block RAINBOW = new RainbowBlock();
+    public static final Block blocksound = new BlockSound();
     
     @EventHandler
     public void init(FMLInitializationEvent event) {
         GameRegistry.registerBlock(new MyBlock(), "myblock");
+        GameRegistry.addShapelessRecipe(new ItemStack(RAINBOW), new ItemStack(Blocks.dirt));
+        GameRegistry.registerBlock(blocksound, "BlockSound");
+
+        MinecraftForge.EVENT_BUS.register(new BlockBreakEventHandler());
+
 
         customSword();
+        customFood();
+
+        GameRegistry.registerBlock(new BlockRedstoneInput(), "redstone_input");
+        GameRegistry.registerBlock(new BlockRedstoneClock(), "redstone_clock");
     }
 
     public void customSword(){
@@ -32,5 +48,16 @@ public class ExampleMod
                 "B",
                 'A',new ItemStack(Items.diamond_sword),
                 'B',Items.gunpowder );
+    }
+
+    public void customFood(){
+
+        PotionEffect[] onigiri ={
+                new PotionEffect(Potion.regeneration.id, 1200, 1),
+                new PotionEffect(Potion.damageBoost.id, 1200, 1),
+                new PotionEffect(Potion.moveSpeed.id, 1200, 1),
+                new PotionEffect(Potion.jump.id, 600, 1)
+        };
+        GameRegistry.registerItem(new ItemModFood("Onigiri", 1, 0.5f, false, onigiri).setAlwaysEdible(), "Onigiri");
     }
 }
